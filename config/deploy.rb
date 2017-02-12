1d8e05e1e55363bbc0bfc26eabeb0c74323acc96
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-02-13 01:30:39 +0800

    Change deploy branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:spencerwuwu/art.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'rails'
+# set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/art/"
