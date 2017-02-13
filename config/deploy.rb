930cad33a23b184593904d7cc4823872f0736f6d
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-02-13 21:33:51 +0800

    Set up staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,6 @@ set :repo_url, "git@github.com:spencerwuwu/art.git"
 # set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/art/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@ server '45.118.133.194', user: 'deploy', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+set :deploy_to, "/home/deploy/art/"
 
 
 
