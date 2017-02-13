481b807b1bd3a3ca73cffbffd48c81f5fd35170c
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-02-14 00:34:12 +0800

    Add sqlite database to linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:spencerwuwu/art.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/secrets.yml"
+append :linked_files, "config/secrets.yml","db/production.sqlite3", "db/staging.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
