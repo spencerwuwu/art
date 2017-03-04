3167545a41cd500f24c4cd36eb3fb462f601f8ee
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2017-03-05 01:10:36 +0800

    Change csrf setting in admin controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,6 +6,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    protect_from_forgery with: :reset_session
     before_action :authenticate_admin
 
     def authenticate_admin
