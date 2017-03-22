f1020d69fdd44399c3c1fe26a8d61de481866f97
Wei-Cheng Wu <spencerwu85@gmail.com>
2017-03-22 22:17:11 +0800

    Upload zine

---
+++ public/zine/static/js/subpage.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+  var w = $(window).width();
+  var h = $(window).height();
+
+  var cal_w = 4096/2304 * h;
+  $("#fixed-img").css("width", cal_w);
+  $("#fixed-img").css("height", h);
+  $("#fixed-img").css("top", 0);
+  var left = w/2 - cal_w/2;
+  $("#fixed-img").css("left", left);
+
+  if ( w >= h ){
+    $('#bkg-img-n').css("display", "none");
+    $('#bkg-img-n1').css("display", "none");
+    $('#bkg-img-n2').css("display", "none");
+	  if ( h/w >= 2304/4096 ){ 
+    $('#bkg-img-w2').css("display", "block");
+    $('#bkg-img-w1').css("display", "none");
+	  }
+	  else{
+    $('#bkg-img-w2').css("display", "none");
+    $('#bkg-img-w1').css("display", "block");
+	  }
+  }
+  else {
+    $('#bkg-img-w1').css("display", "none");
+    $('#bkg-img-w2').css("display", "none");
+    $('#bkg-img-n').css("display", "block");
+    $('#bkg-img-n1').css("display", "block");
+    $('#bkg-img-n2').css("display", "block");
+  }
+
+  $(".page-control").removeClass("showup");
+
+
+});
+
+
+
+$(window).resize(function(){
+
+
+  var w = $(window).width();
+  var h = $(window).height();
+  var cal_w = 4096/2304 * h;
+  $("#fixed-img").css("width", cal_w);
+  $("#fixed-img").css("height", h);
+  $("#fixed-img").css("top", 0);
+  var left = w/2 - cal_w/2;
+  $("#fixed-img").css("left", left);
+
+  if ( w >= h ){
+    $('#bkg-img-n').css("display", "none");
+    $('#bkg-img-n1').css("display", "none");
+    $('#bkg-img-n2').css("display", "none");
+	  if ( h/w >= 2304/4096 ){ 
+    $('#bkg-img-w2').css("display", "block");
+    $('#bkg-img-w1').css("display", "none");
+	  }
+	  else{
+    $('#bkg-img-w2').css("display", "none");
+    $('#bkg-img-w1').css("display", "block");
+	  }
+  }
+  else {
+    $('#bkg-img-w1').css("display", "none");
+    $('#bkg-img-w2').css("display", "none");
+    $('#bkg-img-n').css("display", "block");
+    $('#bkg-img-n1').css("display", "block");
+    $('#bkg-img-n2').css("display", "block");
+  }
+
+
+
+
+});
